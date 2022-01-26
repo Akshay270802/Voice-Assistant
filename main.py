4f6e2c22c6dea36227fecc70797a7b1d8c55879b
Akshay270802 <78205714+Akshay270802@users.noreply.github.com>
2022-01-26 14:13:00 +0530

    Final commit

--- main.py
+++ main.py
@@ -1,101 +1,168 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import speech_recognition as sr
+import pyttsx3
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia
 import webbrowser
 import os
-import smtplib
+import time
+import subprocess
+import json
+import requests
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
+print('Loading your AI personal assistant - G One')
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
 
 
-def speak(audio):
-    engine.say(audio)
+def speak(text):
+    engine.say(text)
     engine.runAndWait()
 
-
 def wishMe():
-    hour = int(datetime.datetime.now().hour)
+    hour=datetime.datetime.now().hour
     if hour>=0 and hour<12:
-        speak("Good Morning!")
-
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
     elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
-
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
     else:
-        speak("Good Evening!")  
-
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
-
-    r = sr.Recognizer()
+    r=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
+        audio=r.listen(source)
 
-    try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
 
-    except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        return "None"
-    return query
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('Your E-mail', 'Your Password') 
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
+speak("Loading your AI personal assistant. Please keep patience")
+wishMe()
 
-if __name__ == "__main__":
-    wishMe()
-    if 1:
-        query = takeCommand().lower()
 
-        if 'wikipedia' in query:
+if __name__=='__main__':
+
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your personal assistant is shutting down,Good bye')
+            print('your personal assistant is shutting down,Good bye')
+            break
+
+
+
+        if 'wikipedia' in statement:
             speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
             speak("According to Wikipedia")
             print(results)
             speak(results)
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("youtube is open now")
+            time.sleep(5)
 
-        elif 'open google' in query:
-            webbrowser.open("google.com")
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google chrome is open now")
+            time.sleep(5)
 
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Google Mail open now")
+            time.sleep(5)
+
+        elif "weather" in statement:
+            api_key="8ef61edcf1c576d65d836254e11ea420"
+            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            speak("whats the city name")
+            city_name=takeCommand()
+            complete_url=base_url+"appid="+api_key+"&q="+city_name
+            response = requests.get(complete_url)
+            x=response.json()
+            if x["cod"]!="404":
+                y=x["main"]
+                current_temperature = y["temp"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                speak(" Temperature in kelvin unit is " +
+                      str(current_temperature) +
+                      "\n humidity in percentage is " +
+                      str(current_humidiy) +
+                      "\n description  " +
+                      str(weather_description))
+                print(" Temperature in kelvin unit = " +
+                      str(current_temperature) +
+                      "\n humidity (in percentage) = " +
+                      str(current_humidiy) +
+                      "\n description = " +
+                      str(weather_description))
+
+            else:
+                speak(" City Not Found ")
 
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
 
-        elif 'open code' in query:
-            codePath = "C:\\Program Files\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+        elif 'time' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"the time is {strTime}")
 
-        elif 'email to name' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "your e-mail address"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry, I'm not able to send this email") 
+        elif 'who are you' in statement or 'what can you do' in statement:
+            speak('I am Jarvis version 1 point O your persoanl assistant. I am programmed to minor tasks like'
+                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
+                  'in different cities , get top headline news from times of india and you can ask me computational or geographical questions too!')
+
+
+        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+            speak("I was built by Akshay Panchal")
+            print("I was built by Akshay Panchal")
+
+        elif "open stackoverflow" in statement:
+            webbrowser.open_new_tab("https://stackoverflow.com/login")
+            speak("Here is stackoverflow")
+
+        elif 'news' in statement:
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            speak('Here are some headlines from the Times of India,Happy reading')
+            time.sleep(6)
+
+        elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(5)
+
+        elif 'ask' in statement:
+            speak('I can answer to computational and geographical questions and what question do you want to ask now')
+            question=takeCommand()
+            app_id="R2K75H-7ELALHR35X"
+            client = wolframalpha.Client('6L4A7G-2GVWLLU7H8')
+            res = client.query(question)
+            answer = next(res.results).text
+            speak(answer)
+            print(answer)
+
+
+        elif "log off" in statement or "sign out" in statement:
+            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+
+time.sleep(3)
